81187b848e7ab85b6159ed75cda51f5c752c2183
+ <lucy.njoroge1@student.moringaschool.com>
2023-06-22 12:03:16 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(record) {
+    const winRecord = record.find(obj => obj.result === "W");
+    return winRecord ? winRecord.year : undefined;
+  }
+  
